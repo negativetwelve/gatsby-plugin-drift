228ccae06d78f7ca6fcbce5fe75c19f051fef002
Gareth Bk <garethbk@gmail.com>
2019-08-26 10:32:14 -0700

    Add option to plugin config to enable/disable chat widget in development mode, with default parameters of false; update readme accordingly.

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,5 +1,13 @@
-exports.onInitialClientRender = () => {
-  if (process.env.NODE_ENV === 'production' && typeof drift === 'object' && window.driftAppId) {
+exports.onInitialClientRender = (
+  _,
+  pluginOptions = { enableDuringDevelop: false }
+) => {
+  if (
+    (pluginOptions.enableDuringDevelop ||
+      process.env.NODE_ENV === 'production') &&
+    typeof drift === 'object' &&
+    window.driftAppId
+  ) {
     window.drift.SNIPPET_VERSION = '0.3.1';
     window.drift.load(window.driftAppId);
   }
